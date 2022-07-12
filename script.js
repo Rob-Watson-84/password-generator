62f7efad97b51a634ee5f2486d557b2c9ceb0d5d
Rob-Watson-84 <106341062+Rob-Watson-84@users.noreply.github.com>
2022-07-12 15:38:41 -0700

    fixed code. forgot to add prompts

--- script.js
+++ script.js
@@ -1,14 +1,70 @@
-var password=document.getElementById("password");
+var generateBtn = document.querySelector("#button");
+console.log(generateBtn)
 
-function genPassword() {
-  var character = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 16;
-  var password = "";
+var lcase ="abcdefghijklmnopqrstuvwxyz";
 
- for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * character.length);
-   password += character.substring(randomNumber, randomNumber +1);
+var ucase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var char ="!@#$%^&*()=+<>";
+
+var num = "0123456789";
+
+var allChar = "";
+
+// Prompts for generating password
+function generatePassword() {
+  var pwdLength = prompt(
+    "How long would you like your password to be? Please specify between 8 and 128 characters."
+  );
+  if (pwdLength < 8 || pwdLength > 128 || isNaN(parseInt(pwdLength))) {
+    alert("Please enter a number between 8 and 128.");
+  } else {
+    var lowerCase = confirm("Would you like to use lowercase letters?");
+    if(lowerCase){
+      allChar += lcase
+    };
+
+    var upperCase = confirm("Would you like to use uppercase letters?");
+    if (upperCase){
+      allChar += ucase
+    };
+
+    var specChar = confirm("Would you like to use special characters?");
+    if (specChar){
+      allChar += char
+    };
+
+    var numbers = confirm("Would you like to use numbers?");
+    if (num){
+      allChar += num
+    };
+
+    if (
+      lowerCase === false &&
+      upperCase === false &&
+      specChar === false &&
+      numbers === false
+    ) {
+      alert("You must select at least one character type.");
+      generatePassword();
+    }
   }
-  
-  document.getElementById("password").value = password;
- }
+
+  var pwd = "";
+  for (let i=0; i<pwdLength; i++){
+    pwd += allChar.charAt(Math.floor(Math.random() * allChar.length))
+  }
+  return pwd
+}
+
+
+function writePassword() {
+
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+
+generateBtn.addEventListener("click", writePassword);
+console.log(generateBtn)
